ead6e80890256b0e649940535a971b71018a5a4b
Mazur Roman <superioritywork@gmail.com>
2023-05-21 13:34:20 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,75 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 import css from './App.module.css';
-export class App extends Component {
-  state = {
-    contacts: [
+
+export function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('localContacts')) ?? '',
+    ...[
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    ]
+  );
+  const [filter, setFilter] = useState('');
+  const handleNameChange = event => {
+    const { value } = event.target;
+    setFilter(value);
   };
-
-  handleNameChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-  handleBtnNameSubmit = (nameData, numberData) => {
-    const { contacts } = this.state;
+  const handleBtnNameSubmit = (nameData, numberData) => {
     const isInclude = contacts.find(contact => contact.name === nameData);
 
     if (isInclude) {
       Report.info(nameData + ' Is already in contacts!');
       return;
     }
-    this.setState(prevState => ({
-      contacts: [
-        ...prevState.contacts,
-        { name: nameData, id: nanoid(), number: numberData },
-      ],
-    }));
+    setContacts(prevState => [
+      ...prevState,
+      { name: nameData, id: nanoid(), number: numberData },
+    ]);
   };
-  getVisibleName = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleName = () => {
     const normilizeFilter = filter.toLocaleLowerCase();
     return contacts.filter(contact =>
       contact.name.toLocaleLowerCase().includes(normilizeFilter)
     );
   };
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
   };
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('localContacts');
-    const parsedContacts = JSON.parse(savedContacts);
-    if (parsedContacts) {
-      this.setState({
-        contacts: parsedContacts,
-      });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('localContacts', JSON.stringify(contacts));
-    }
-  }
-  render() {
-    const { filter } = this.state;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <div className={css.appContainer}>
-          <div>
-            <h1 className={css.title}>Phonebook</h1>
-            <ContactForm handleBtnNameSubmit={this.handleBtnNameSubmit} />
-          </div>
-          <div>
-            <h2 className={css.subtitle}>Contacts</h2>
-            <Filter value={filter} onChange={this.handleNameChange} />
-            <ContactList
-              getVisibleName={this.getVisibleName}
-              deleteContact={this.deleteContact}
-            />
-          </div>
+  useEffect(() => {
+    localStorage.setItem('localContacts', JSON.stringify(contacts));
+  }, [contacts]);
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div className={css.appContainer}>
+        <div>
+          <h1 className={css.title}>Phonebook</h1>
+          <ContactForm handleBtnNameSubmit={handleBtnNameSubmit} />
+        </div>
+        <div>
+          <h2 className={css.subtitle}>Contacts</h2>
+          <Filter value={filter} onChange={handleNameChange} />
+          <ContactList
+            getVisibleName={getVisibleName}
+            deleteContact={deleteContact}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
