48446b4f6f059967eaad2bdea290606bf02b8331
Mazur Roman <superioritywork@gmail.com>
2023-05-21 13:58:18 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,7 @@ import css from './App.module.css';
 
 export function App() {
   const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('localContacts')) ?? '',
-    ...[
+    JSON.parse(window.localStorage.getItem('localContacts')) ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
